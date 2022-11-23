3dd5e1964955a1194947685dc8264c403956cbd2
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-11-23 23:58:58 +0800

    Create packages.sh

---
+++ packages.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo apt update && sudo apt upgrade -y
+sudo apt install neovim nano nodejs npm python3-pip cfdisk curl wget build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev ffmpeg
